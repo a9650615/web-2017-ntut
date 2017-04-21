8b3b6f08aa5db67829fe2a6e87bf6cb035f25ad7
菜鳥祐 <a9650615@gmail.com>
2017-04-21 14:14:54 +0800

    取消 fullpage.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 'use strict'
 import 'babel-polyfill'
-import {fixNavBar} from './header'
+import './header'
 import './section2'
 import '../styles/index.css'
 
@@ -10,22 +10,22 @@ if (module.hot) {
 
 // var $ = document.querySelector.bind(document)
 // $('#block2').innerHTML = asd
-let $ = window.$
-$(document).ready(() => {
-  $('#fullpage').fullpage({
-    css3: true,
-    easingcss3: 'ease',
-    anchors: ['firstPage', 'secendPage', '3rdPage', '4rdPage'],
-    menu: '#myMenu',
-    // events
-    onLeave: (index, nextIndex, direction) => {
-      // fixNavBar(index, nextIndex, direction)
-    },
-    afterLoad: (anchorLink, index) => { fixNavBar(index, -1, -1) },
-    afterRender: () => {},
-    afterResize: () => {},
-    afterResponsive: (isResponsive) => {},
-    afterSlideLoad: (anchorLink, index, slideAnchor, slideIndex) => {},
-    onSlideLeave: (anchorLink, index, slideIndex, direction, nextSlideIndex) => {}
-  })
-})
+// let $ = window.$
+// $(document).ready(() => {
+//   $('#fullpage').fullpage({
+//     css3: true,
+//     easingcss3: 'ease',
+//     anchors: ['firstPage', 'secendPage', '3rdPage', '4rdPage'],
+//     menu: '#myMenu',
+//     // events
+//     onLeave: (index, nextIndex, direction) => {
+//       // fixNavBar(index, nextIndex, direction)
+//     },
+//     afterLoad: (anchorLink, index) => { fixNavBar(index, -1, -1) },
+//     afterRender: () => {},
+//     afterResize: () => {},
+//     afterResponsive: (isResponsive) => {},
+//     afterSlideLoad: (anchorLink, index, slideAnchor, slideIndex) => {},
+//     onSlideLeave: (anchorLink, index, slideIndex, direction, nextSlideIndex) => {}
+//   })
+// })
