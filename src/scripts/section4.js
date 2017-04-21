c88162beccf3362ffd68e038e50be44a6afc9283
enchang <enchang1017@gamil.com>
2017-04-21 17:21:57 +0800

    調整css section2,section3.4

--- src/scripts/section4.js
+++ src/scripts/section4.js
@@ -3,3 +3,11 @@ import '../styles/section4.css'
 import section4 from '../template/section4.html'
 
 $('#section4').html(section4)
+
+$('.select_button .button').on('click', (e) => {
+  let targetId = $(e.currentTarget).data('target')
+  $('.select_button .button').removeClass('is-active').addClass('is-outlined')
+  $(e.currentTarget).removeClass('is-outlined').addClass('is-active')
+  $('#man_wear_block, #woman_wear_block').addClass('hidden')
+  $(`#${targetId}`).removeClass('hidden')
+})
