da730a99e33f33f0c36bd6e00b84de94aaa9323a
birdyo <a9650615@gmail.com>
2017-04-23 21:16:51 +0800

    section7 加張圖片

---
+++ src/scripts/section5.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import html from '../template/section5.html'
+
+$('#section5').html(html)
--- src/scripts/section7.js
+++ src/scripts/section7.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import '../styles/section7.css'
 import section7 from '../template/section7.html'
 
 $('#section7').html(section7)
