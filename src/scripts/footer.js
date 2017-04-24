0264f722e22ff39c67d8d21747cc3cf1e57b7215
birdyo <a9650615@gmail.com>
2017-04-25 02:58:48 +0800

    暗藏玄機的頁尾

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -6,8 +6,35 @@ $('footer').html(html)
 
 $('.cut').on('click', (e) => {
   $(e.currentTarget).addClass('clicked')
+  $('#fuck-text-2').hide()
   setTimeout(() => {
     $('footer').addClass('open-gate')
     $('footer .on-top').addClass('open-gate')
+    goToHell()
   }, 900)
 })
+
+let goToHell = () => {
+  setTimeout(() => {
+    $('#surprise-mother-fucker').fadeIn(200).css('display', 'flex')
+    setTimeout(() => {
+      $('.fuck-text div').hide()
+      $('#fuck-text-2').fadeIn(200)
+      seeYouNotAgain()
+    }, 2000)
+  }, 500)
+}
+
+let seeYouNotAgain = () => {
+  setTimeout(() => {
+    $('#fullpage').addClass('get-out')
+    $('body').addClass('fix')
+    setTimeout(() => {
+      $('.door').addClass('ease')
+      setTimeout(() => {
+        $('.door').removeClass('ease').addClass('open').addClass('ease')
+        window.location.href = 'https://aqua.birdyo.ga'
+      }, 400)
+    }, 1000)
+  }, 1000)
+}
