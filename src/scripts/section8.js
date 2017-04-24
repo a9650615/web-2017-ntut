e92269592cb1e77d6f7181bbda591c9fbd91e139
birdyo <a9650615@gmail.com>
2017-04-24 23:05:29 +0800

    section8 跑馬燈

--- src/scripts/section8.js
+++ src/scripts/section8.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
-import '../styles/section6.css'
+import '../styles/section8.css'
 import section8 from '../template/section8.html'
 
 $('#section8').html(section8)
+
+$('.img_block .silder').hide()
+
+let index = 0
+setInterval(() => {
+  index++
+  if (index >= 4) {
+    index = 0
+  }
+  $('.img_block .silder').fadeOut(100)
+  $('.img_block .silder').eq(index).fadeIn(300)
+}, 2000)
