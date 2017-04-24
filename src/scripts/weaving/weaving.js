2f7e1246764b109380055f512f0dbc91f93c6174
Unknown <enchang1017@gmail.com>
2017-04-24 08:02:41 +0800

    新增分頁weaving

---
+++ src/scripts/weaving/weaving.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+import html1 from '../../template/weaving/weaving1.html'
+import html2 from '../../template/weaving/weaving2.html'
+import html3 from '../../template/weaving/weaving.html'
+
+$('#weaving1').html(html1)
+$('#weaving2').html(html2)
+$('#weaving3').html(html3)
